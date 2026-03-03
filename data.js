6d978bde25faebaee41df4d4a3d21281cd95e1bc
Marat Parashchenko <maratnyancat@gmail.com>
2026-03-03 12:52:26 +0100

    test

---
+++ data.js
@@ -0,0 +1,13 @@
+function add(a, b) {
+    return a + b;
+}
+
+window.test = {
+    constants: {
+        something: 123,
+        somethingElse: 'abc',
+    },
+    functions: {
+        add: add,
+    }
+}
